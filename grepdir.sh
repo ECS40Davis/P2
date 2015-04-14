0243c4bbfedd87776bbcc5076f5cace225541a9c
MaJessica9 <jamma@ucdavis.edu>
2015-04-13 20:04:36 -0700

    Update grepdir.sh

--- grepdir.sh
+++ grepdir.sh
@@ -4,14 +4,14 @@ if [[ $# -le 1 || ! -d $1 ]]; then
   echo 'usage: grepdir.sh directory pattern [-grep option]*'
 else
   if [[ -z $3 ]]; then
-    find #1 -exec grep $2 {} \; 2> /dev/null
-  if [[ $3 && $3 != -* ]]; then
+    find $1 -exec grep $2 {} \; 2> /dev/null
+  elif [[ $# == 3 && $3 != -* ]]; then
     echo 'usage: grepdir.sh directory pattern [-grep option]*'
-  elif [[ $3 && $3 == -* ]]; then
-    find #1 -exec grep $2 $3 {} \; 2> /dev/null
-  elif [[ $4 && $4 != -* ]]; then
+  elif [[ $# == 3 && $3 == -* ]]; then
+    find $1 -exec grep $2 $3 {} \; 2> /dev/null
+  elif [[ $# == 4 && $4 != -* ]]; then
     echo 'usage: grepdir.sh directory pattern [-grep option]*'
-  elif [[ $4 && $4 == -* ]]; then
-    find #1 -exec grep $2 $3 $4 {} \; 2> /dev/null
+  elif [[ $# == 4 && $4 == -* ]]; then
+    find $1 -exec grep $2 $3 $4 {} \; 2> /dev/null
   fi
 fi
